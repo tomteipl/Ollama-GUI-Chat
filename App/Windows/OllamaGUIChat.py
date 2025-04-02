581b1eb0608ea20a6aa1a99e2f12d9c0ee2ce7a8
tomteipl <tomteipl@gmail.com>
2025-04-02 18:21:15 +0200

    OGC v2.0.0: Windows update

--- App/Windows/OllamaGUIChat.py
+++ App/Windows/OllamaGUIChat.py
@@ -4,9 +4,65 @@ import json
 import customtkinter as ctk
 import os
 import threading as thr
+import configparser
 
 APP_PATH: str = os.path.dirname(os.path.realpath(__file__))
-THEME_PATH: str = os.path.join(APP_PATH, "theme", "custom-theme.json")
+THEME_PATH: str = os.path.join(APP_PATH, "theme/")
+
+settings = configparser.ConfigParser()
+settings_file_path = "settings.ini"
+
+
+def set_settings():
+    try:
+        if os.path.exists(settings_file_path):
+            settings.read(settings_file_path)
+
+            theme = settings["Settings"].get("theme")
+            appearance = settings["Settings"].get("appearance")
+
+            try:
+                ctk.set_default_color_theme(THEME_PATH + f"{theme}")
+
+            except Exception as e:
+                print(f"Error while reading theme: {e}")
+                ctk.set_default_color_theme(THEME_PATH + "default.json")
+                settings["Settings"]["theme"] = "default.json"
+
+                with open(settings_file_path, "w") as cf:
+                    settings.write(cf)
+
+            try:
+                ctk.set_appearance_mode(str(appearance))
+
+            except Exception as e:
+                print(f"Error while reading appearance mode: {e}")
+                ctk.set_appearance_mode("light")
+                settings["Settings"]["appearance"] = "light"
+
+                with open(settings_file_path, "w") as cf:
+                    settings.write(cf)
+
+        else:
+            ctk.set_default_color_theme(THEME_PATH + "default.json")
+            ctk.set_appearance_mode("light")
+            settings["Settings"] = {
+                "theme": "default.json",
+                "host": "http://localhost:11434",
+                "appearance": "light"
+            }
+
+            with open(settings_file_path, "w") as cf:
+                settings.write(cf)
+
+    except Exception as e:
+        print(f"Error settings.ini: {e}")
+
+set_settings()
+
+theme = settings["Settings"].get("theme")
+appearance = settings["Settings"].get("appearance")
+host = settings["Settings"].get("host")
 
 error_log = []
 model_list = []
@@ -17,6 +73,8 @@ class GPLLicense(ctk.CTkToplevel):
 
         self.title("GPL License")
         self.geometry("500x300")
+        self.resizable(False, False)
+        self.attributes("-topmost", True)
 
         self.label = ctk.CTkLabel(self, text="""
 Copyright (c) 2025 by Kamil Wiśniewski <tomteipl@gmail.com>
@@ -44,6 +102,8 @@ class ErrorWindow(ctk.CTkToplevel):
 
         self.title("Error logs")
         self.geometry("600x400")
+        self.resizable(False, False)
+        self.attributes("-topmost", True)
 
         self.error_output = ctk.CTkTextbox(self, height=390, width=590, cursor="arrow")
         self.error_output.grid(row=0, column=0, padx=5, pady=5, sticky="nsew",)
@@ -52,24 +112,97 @@ class ErrorWindow(ctk.CTkToplevel):
         self.error_output.insert("1.0", error_lines)
         self.error_output.configure(wrap="word", state="disabled", font=("", 14))
 
-class OllamaGUIChat(ctk.CTk):
+class SelectTheme(ctk.CTkToplevel):
     def __init__(self):
-
-        try:
-            ctk.set_default_color_theme(THEME_PATH)
-        except Exception as e:
-            ctk.set_default_color_theme("blue")
-            print(f"Error: {e}")
-            error_log.append(f"Error: {e}")
-
         super().__init__()
 
-        ctk.set_appearance_mode("light")
+        self.title("Select Theme")
+        self.geometry("500x330")
+        self.resizable(False, False)
+        self.attributes("-topmost", True)
+
+        top_frame = ctk.CTkFrame(self)
+        top_frame.grid(row=0, padx=5, pady=2, sticky="we")
+        top_frame.configure(fg_color="transparent")
+
+        self.button_1 = ctk.CTkButton(top_frame, text="Gruvbox", command=lambda: self.theme_file("gruvbox.json"))
+        self.button_1.grid(row=0, column=0,)
+        self.button_1.configure(font=("", 14), fg_color=["#d79921", "#b57614"],
+            hover_color=["#cc241d", "#9d0006"],
+            border_color=["#3c3836", "#ebdbb2"],
+            text_color=["#FFFFFF", "#FFFFFF"],
+            )
+        self.button_1_textbox = ctk.CTkTextbox(top_frame, cursor="arrow")
+        self.button_1_textbox.grid(row=0, column=1, sticky="we", pady=5)
+        self.button_1_textbox.insert("end" ,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.")
+        self.button_1_textbox.configure(height=80, width=350, state="disabled",
+            fg_color=["#ebdbb2", "#3c3836"],
+            border_color=["#3c3836", "#ebdbb2"],
+            text_color=["#282828", "#FFFFFF"],
+            )
+
+        self.button_2 = ctk.CTkButton(top_frame, text="Default", command=lambda: self.theme_file("default.json"))
+        self.button_2.grid(row=1, column=0)
+        self.button_2.configure(font=("", 14),
+            fg_color=["#978dfd", "#191a19"],
+            hover_color=["#6f68bd", "#4b4d4b"],
+            border_color=["#3a3666", "#02b508"],
+            text_color=["#FFFFFF", "#00ff04"],
+            )
+        self.button_2_textbox = ctk.CTkTextbox(top_frame, cursor="arrow")
+        self.button_2_textbox.grid(row=1, column=1, sticky="we", pady=5)
+        self.button_2_textbox.insert("end" ,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.")
+        self.button_2_textbox.configure(height=80, width=350, state="disabled",
+            fg_color=["#7d76d3", "#141414"],
+            border_color=["#3a3666", "#02b508"],
+            text_color=["#FFFFFF", "#00ff04"],
+            )
+
+        self.button_3 = ctk.CTkButton(top_frame, text="Tokyo Night", command=lambda: self.theme_file("tokyo_night.json"))
+        self.button_3.grid(row=2, column=0)
+        self.button_3.configure(font=("", 14),
+            fg_color=["#2959aa", "#7aa2f7"],
+            hover_color=["#8c4351", "#f7768e"],
+            border_color=["#5a3e8e", "#565f89"],
+            text_color=["#FFFFFF", "#24283b"],
+            )
+        self.button_3_textbox = ctk.CTkTextbox(top_frame, cursor="arrow")
+        self.button_3_textbox.grid(row=2, column=1, sticky="we", pady=5)
+        self.button_3_textbox.insert("end" ,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.")
+        self.button_3_textbox.configure(height=80, width=350, state="disabled",
+            fg_color=["#e6e7ed", "#414868"],
+            border_color=["#5a3e8e", "#565f89"],
+            text_color=["#5a3e8e", "#bb9af7"],
+            )
+        
+        self.restart_label = ctk.CTkLabel(self, text="Restart after select!")
+        self.restart_label.grid(row=3, column=0, columnspan=2, sticky="nswe", pady=10)
+        self.restart_label.configure(font=("", 20))
+
+    def theme_file(self, theme_name=None):
+        if theme_name:
+            settings.read(settings_file_path)
+
+            if "Settings" not in settings:
+                settings["Settings"] = {}
+
+            settings["Settings"]["theme"] = theme_name
+            with open(settings_file_path, "w") as cf:
+                settings.write(cf)
+        else:
+            print("Theme not selected")
+
+        self.destroy()
+
+class OllamaGUIChat(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
         self.messages = []
         self.full_reply = ""
         self.gpl_opened = None
         self.error_opened = None
+        self.theme_opened = None
         self.response = None
 
         self.title("Ollama GUI Chat")
@@ -109,10 +242,10 @@ class OllamaGUIChat(ctk.CTk):
         self.host_url = ctk.CTkEntry(top_frame2)
         self.host_url.grid(row=0, column=0, padx=(20,3), sticky="e")
         self.host_url.configure(height=20, width=230, font=("", 12), placeholder_text="Host URL...")
-        self.host_url.insert(0 ,"http://localhost:11434")
+        self.host_url.insert(0 , host)
 
-        self.theme_var = ctk.StringVar(value="off")
-        self.theme_switch = ctk.CTkSwitch(top_frame2, text="light/dark mode", variable=self.theme_var, onvalue="on", offvalue="off", command=self.theme_modes,)
+        self.theme_var = ctk.StringVar(value=appearance)
+        self.theme_switch = ctk.CTkSwitch(top_frame2, text="light/dark mode", variable=self.theme_var, onvalue="dark", offvalue="light", command=self.theme_modes,)
         self.theme_switch.grid(row=0, column=1, padx=(0,5), sticky="e")
 
         # mid panel
@@ -171,17 +304,23 @@ class OllamaGUIChat(ctk.CTk):
         self.send_button.grid(row=3, column=1, sticky="we", padx=(0,5), pady=(5,5))
         self.send_button.configure(height=100, width=45, corner_radius=5, border_width=2)
 
-        self.copyright_label = ctk.CTkLabel(self, text="Copyright © 2025 by Kamil Wiśniewski | Ver. 1.6.0")
+        self.copyright_label = ctk.CTkLabel(self, text="Copyright © 2025 by Kamil Wiśniewski | Ver. 2.0.0")
         self.copyright_label.grid(sticky="se", row=4, column=0, columnspan=2, padx=5)
         self.copyright_label.configure(font=("", 10))
+
+        self.settings_button = ctk.CTkButton(self, text="Change theme", command=self.open_theme_window)
+        self.settings_button.grid(row=4, column=0, padx=5, sticky="w")
+        self.settings_button.configure(height=10, width=10)
 
 
 
         # keybind
         self.custom_model_name.bind("<Return>", self.custom_model)
+        self.custom_model_name.bind("<Control-a>", self.keybinds)
         self.input_field.bind("<Control-a>", self.keybinds)
         self.input_field.bind("<Return>", self.keybinds)
         self.host_url.bind("<Return>", self.check_existing_models)
+        self.host_url.bind("<Control-a>", self.keybinds)
 
         self.check_existing_models()
 
@@ -197,8 +336,19 @@ class OllamaGUIChat(ctk.CTk):
             return "break"
 
         elif event.keysym.lower() == "a" and (event.state & 0x4):  # Ctrl+A
-            self.input_field.tag_add("sel", "1.0", "end")
-            return "break"
+            widget = event.widget
+            if widget == self.input_field._textbox:
+                widget.tag_add("sel", "1.0", "end")
+                return "break"
+            
+            elif widget == self.host_url._entry:
+                widget.select_range(0, "end")
+                return "break"
+            
+            elif widget == self.custom_model_name._entry:
+                widget.select_range(0, "end")
+            
+        return "break"
 
     def change_font_size(self, increment):
         font_size = int(self.chat_font_var.get()) + increment
@@ -243,11 +393,18 @@ class OllamaGUIChat(ctk.CTk):
         self.model_menu.configure(values=model_list)
 
     def theme_modes(self):
-        if self.theme_var.get() == "on":
+        if self.theme_var.get() == "dark":
             ctk.set_appearance_mode("dark")
-
+            settings.read(settings_file_path)
+            settings["Settings"]["appearance"] = "dark"
+            with open(settings_file_path, "w") as cf:
+                settings.write(cf)
         else:
             ctk.set_appearance_mode("light")
+            settings.read(settings_file_path)
+            settings["Settings"]["appearance"] = "light"
+            with open(settings_file_path, "w") as cf:
+                settings.write(cf)
 
     def open_gpl(self):
         if self.gpl_opened is None or not self.gpl_opened.winfo_exists():
@@ -263,6 +420,14 @@ class OllamaGUIChat(ctk.CTk):
         else:
             self.error_opened.destroy()
             self.error_opened = ErrorWindow()
+
+    def open_theme_window(self):
+        if self.theme_opened is None or not self.theme_opened.winfo_exists():
+            self.theme_opened = SelectTheme()
+
+        else:
+            self.theme_opened.destroy()
+            self.theme_opened = None
 
     def clear_chat(self):
         self.chat_output.configure(state="normal")
@@ -287,7 +452,7 @@ class OllamaGUIChat(ctk.CTk):
         file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
 
         if not file_path:       # if you dont want to see an error each time you cancel loading - keep this intact
-           return
+            return
 
         try:
             with open(file_path, "r", encoding="utf-8") as f:
@@ -336,6 +501,13 @@ class OllamaGUIChat(ctk.CTk):
             model_list.extend([model["name"] for model in data["models"]])
             self.model_menu_var.set(model_list[0])
             self.refresh_model_list()
+
+            api_url = self.host_url.get()
+            settings.read(settings_file_path)
+            settings["Settings"]["host"] = api_url
+
+            with open(settings_file_path, "w") as cf:
+                settings.write(cf)
 
         except requests.exceptions.RequestException as e:
             model_list.clear()
@@ -419,5 +591,6 @@ class OllamaGUIChat(ctk.CTk):
             self.send_button.configure(state="normal")
             self.hide_progress()
 
-ollama_gui = OllamaGUIChat()
-ollama_gui.mainloop()
+if __name__ == "__main__":
+    ogc_instance = OllamaGUIChat()
+    ogc_instance.mainloop()
